f74cf7680e39926dda4f4bff42757ac61c9380f5
emersondelonga <emersondelonga@gmail.com>
2017-11-14 10:12:21 -0500

    Update Maze.java

--- src/Maze.java
+++ src/Maze.java
@@ -5,7 +5,7 @@ import java.lang.Exception;
 public class Maze
 {
     private Cell[][] board;
-    private final int DELAY = 200;
+    private final int DELAY = 1;
     private int prevRow = 0;
     private int prevCol = 0;
 
@@ -88,45 +88,82 @@ public class Maze
             return false;
     }
     public static int[][] readMaze() throws Exception {
-            int size = 10;
-            String[][] dataSet = new String[size][size];
-            int[][] numSet = new int[size][size];
-            String fileName = "Workbook1";
-            String extension = ".csv";
-            File inputFile = new File(fileName + extension);
-            Scanner inputObject = new Scanner(inputFile);
-            inputObject.useDelimiter(",");
-            System.out.println("delim is:" + inputObject.delimiter());
-            int row = 0;
-            // process the entire file, with a loop
-            // populate your data structures
-            while (inputObject.hasNextLine() == true) {
-                dataSet[row] = (inputObject.nextLine()).split(",");
-                row++;
-            }
-            for (int r = 0; r < dataSet.length; r++) {
-                for (int c = 0; c < dataSet[1].length; c++) {
-                    dataSet[r][c] = dataSet[r][c].trim();
-                    numSet[r][c] = Integer.parseInt(dataSet[r][c]);
-                }
-                System.out.println();
-            }
-            inputObject.close();
-            return numSet;
-
+        int size = 10;
+        String[][] dataSet = new String[size][size];
+        int[][] numSet = new int[size][size];
+        String fileName = "Workbook1";
+        String extension = ".csv";
+        File inputFile = new File(fileName + extension);
+        Scanner inputObject = new Scanner(inputFile);
+        //inputObject.useDelimiter(",");
+        //System.out.println("delim is:" + inputObject.delimiter());
+        int row = 0;
+        // process the entire file, with a loop
+        // populate your data structures
+        while (inputObject.hasNextLine() == true) {
+            dataSet[row] = (inputObject.nextLine()).split(",");
+            row++;
         }
-    public static void main(String[] args) {
+        for (int r = 0; r < dataSet.length; r++) {
+            for (int c = 0; c < dataSet[0].length; c++) {
+                dataSet[r][c] = dataSet[r][c].trim();
+                System.out.println(dataSet[r][c]);
+                numSet[r][c] = Integer.parseInt(dataSet[r][c].trim());
+            }
+            System.out.println();
+        }
+        inputObject.close();
+        return numSet;
+
+    }
+    public static int[][] generateMaze(int size, double prob) {
+        int maze[][] = new int[size][size];
+        for (int i = 1; i < size - 1; i++) {
+            for (int j = 1; j < size - 1; j++) {
+                maze[i][j] = 1;
+            }
+        }
+        for (int i = 2; i < size - 1; i += 4) {
+            for (int j = 1; j < size - 1; j++) {
+                maze[i][j] = 0;
+            }
+        }
+        for (int i = 1; i < size - 1; i++) {
+            for (int j = 1; j < size - 2; j += 4) {
+                maze[i][j] = 0;
+            }
+        }
+        for (int i = 2; i < size - 1; i+= 2) {
+            for (int j = 1; j < size - 1; j+=1) {
+                if(maze[i][j] == 0)
+                {
+                    if((int)(Math.random()*prob) > 0)
+                    maze[i][j] = 1;
+                }
+            }
+        }
+        maze[0][0] = 1;
+        maze[0][1] = 1;
+        maze[0][2] = 1;
+        maze[0][3] = 1;
+        maze[1][0] = 1;
+        maze[1][1] = 1;
+        maze[1][2] = 1;
+        maze[1][3] = 1;
+        maze[size - 1][size - 1] = 1;
+        maze[size - 1][size - 2] = 1;
+        maze[size - 2][size - 1] = 1;
+        maze[size - 2][size - 2] = 1;
+        maze[size - 3][size - 1] = 1;
+        maze[size - 3][size - 2] = 1;
+        maze[size - 4][size - 1] = 1;
+        maze[size - 4][size - 2] = 1;
+        return maze;
+    }
+
+    public static void main(String[] args) throws Exception{
         StdDraw.enableDoubleBuffering();
-        int[][] maze ={{1,1,0,0,0,0,0,0,0,0},
-                {0,1,1,1,1,0,1,1,1,0},
-                {0,1,1,1,1,0,1,1,0,0},
-                {0,1,0,1,1,1,1,1,1,0},
-                {0,1,0,0,0,0,0,1,1,0},
-                {0,1,1,0,1,1,1,1,1,0},
-                {0,0,1,0,0,1,0,1,0,0},
-                {0,1,1,0,1,1,0,1,1,0},
-                {0,1,1,0,1,1,0,1,1,0},
-                {0,0,0,0,0,0,0,0,1,1}};
+        int[][] maze = generateMaze(100,1.6);
         Maze geerid = new Maze(maze.length, maze[0].length, maze);
         geerid.draw();
         geerid.findPath(0, 0);
